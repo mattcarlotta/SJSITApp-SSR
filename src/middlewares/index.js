69d69085b023033eb53b3d7d71ba96df7631c2d7
Matt <carlotta.matt@gmail.com>
2020-04-26 15:40:24 -0700

    Remove unused morgan config

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,31 +2,13 @@
 import morgan from "morgan";
 import session from "cookie-session";
 import passport from "passport";
-import moment from "~utils/momentWithTZ";
 import { sendError } from "~utils/helpers";
 
-moment.tz.setDefault("America/Los_Angeles");
-
 const { cookieKey, inDevelopment, inProduction, inStaging } = process.env;
-
-const logging = inProduction
-	? ":remote-addr [:date] :referrer :method :url HTTP/:http-version :status :res[content-length]"
-	: "tiny";
 
 export default next => (req, res) => {
 	return new Promise(async resolve => {
 		try {
-			morgan.token("date", () => moment().format("MMMM Do YYYY, h:mm:ss a"));
-			if (inProduction) {
-				morgan.token(
-					"remote-addr",
-					req =>
-						req.headers["x-real-ip"] ||
-						req.headers["x-forwarded-for"] ||
-						req.connection.remoteAddress,
-				);
-			} // logs real ip address
-
 			const middlewares = [
 				session({
 					path: "/",
