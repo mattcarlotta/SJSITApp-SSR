b3177cd767fa037bccf37746531e2700287437ae
Matt <carlotta.matt@gmail.com>
2020-04-29 10:40:42 -0700

    Remove hardcorded withCreds

--- src/sagas/Auth/index.js
+++ src/sagas/Auth/index.js
@@ -176,9 +176,7 @@ export function* updateUserAvatar({ form, id }) {
 	try {
 		yield put(resetServerMessage());
 
-		const res = yield call(avatarAPI.put, `update/${id}`, form, {
-			withCredentials: true,
-		});
+		const res = yield call(avatarAPI.put, `update/${id}`, form);
 		const data = yield call(parseData, res);
 
 		const { avatar, message } = data;
