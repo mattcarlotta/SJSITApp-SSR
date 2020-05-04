e395e8cd6fcf205584ebff0bdaab40619e8411bf
Matt <carlotta.matt@gmail.com>
2020-05-04 12:16:42 -0700

    Fix parseFields test

--- src/utils/parseFields/__tests__/parseFields.test.js
+++ src/utils/parseFields/__tests__/parseFields.test.js
@@ -31,6 +31,17 @@ describe("Parse Fields Helper", () => {
 	});
 
 	it("parses an array of fields", () => {
+		const datesRanges = [
+			moment(new Date("2019-12-17T01:00:00")),
+			moment(new Date("2019-12-17T02:00:00")),
+		];
+
+		const callTimes = [
+			moment(new Date("2019-12-17T01:00:00")),
+			moment(new Date("2019-12-17T02:00:00")),
+			moment(new Date("2019-12-17T03:00:00")),
+		];
+
 		const fields = [
 			{
 				name: "email",
@@ -45,25 +56,22 @@ describe("Parse Fields Helper", () => {
 			{
 				name: "seasonDuration",
 				type: "range",
-				value: [
-					moment(new Date("2019-12-17T01:00:00")),
-					moment(new Date("2019-12-17T02:00:00")),
-				],
+				value: datesRanges,
 			},
 			{
 				name: "callTime1",
 				type: "time",
-				value: moment(new Date("2019-12-17T01:00:00")),
+				value: callTimes[0],
 			},
 			{
 				name: "callTime2",
 				type: "time",
-				value: moment(new Date("2019-12-17T02:00:00")),
+				value: callTimes[1],
 			},
 			{
 				name: "callTime3",
 				type: "time",
-				value: moment(new Date("2019-12-17T03:00:00")),
+				value: callTimes[2],
 			},
 			{
 				name: "0123456789",
@@ -84,14 +92,11 @@ describe("Parse Fields Helper", () => {
 			expect.objectContaining({
 				email: "test@example.com",
 				password: "12345",
-				seasonDuration: [
-					"2019-12-17T01:00:00-08:00",
-					"2019-12-17T02:00:00-08:00",
-				],
+				seasonDuration: [datesRanges[0].format(), datesRanges[1].format()],
 				callTimes: [
-					"2019-12-17T01:00:00-08:00",
-					"2019-12-17T02:00:00-08:00",
-					"2019-12-17T03:00:00-08:00",
+					callTimes[0].format(),
+					callTimes[1].format(),
+					callTimes[2].format(),
 				],
 				responses: [
 					{
